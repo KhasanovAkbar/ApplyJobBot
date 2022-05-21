25ad498d67bf90f040da14db4c274cadb6ddec89
Khasanov Akbar <akbar.hasanov0630@gmail.com>
2022-05-21 21:48:46 +0500

    integration with heroku postgresql
    Signed-off-by: Khasanov Akbar <akbar.hasanov0630@gmail.com>

---
+++ src/main/java/univ/tuit/applyjobbot/ApplyJobBotApplication.java
@@ -0,0 +1,13 @@
+package univ.tuit.applyjobbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyJobBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplyJobBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/univ/tuit/applyjobbot/ApplyJobBotApplicationTests.java
@@ -0,0 +1,13 @@
+package univ.tuit.applyjobbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyJobBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
